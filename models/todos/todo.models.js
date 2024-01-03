289987b1f3d6bc5522264ed3f8fb497ca27e616f
Abhisheksingh734 <114075050+Abhisheksingh734@users.noreply.github.com>
2024-01-03 19:29:34 +0530

    Initial commit

---
+++ models/todos/todo.models.js
@@ -0,0 +1,29 @@
+import mongoose, { mongo } from 'mongoose';
+
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    //Array of subTodos
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'subTodo',
+      },
+      {},
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Todo = mongoose.model('Todo', todoSchema);
